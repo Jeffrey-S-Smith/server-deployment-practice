8fe20d0cc1897b0513bba97fe105392a22f016dd
Jeffrey Smith <jeffrey_s_smith@live.com>
2022-09-19 21:07:20 -0700

    update

---
+++ index.js
@@ -0,0 +1,8 @@
+'use strict';
+require('dotenv').config();
+const app = require('./server.js');
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log('app is running');
+});
